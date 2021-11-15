2c67eed8f15f68f8e72deba3c5aedd91201a1887
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2021-11-16 00:22:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+console.log(searchBox);
+
+searchBox.addEventListener('input' , onInput);
+
+function onInput() {};
